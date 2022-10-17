957e78de31e089b6301872e93a63daa39291b992
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-10-17 16:50:32 +0300

    DONE

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer()
+   puts  "Hello, programmer!"
+end
+
+greet_programmer()
+
+def greet(name)
+     puts "Hello, #{name}!"
+end 
+ 
+
+def greet_with_default(name = "programmer")
+     puts "Hello, #{name}!"
+end 
+
+def add(num1, num2)
+    return 2 + 5
+end 
+
+def halve(number)
+     if number.class != Integer
+          return nil
+     end 
+    return  number / 2
+end 
